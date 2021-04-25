e88f4bdf215588d51bb129621125c96074d244aa
Jefferson Lee <jeffersonlee@Jeffersons-MacBook-Pro.local>
2021-04-25 11:53:59 -0700

    ray class and lerped background image

--- main.py
+++ main.py
@@ -3,20 +3,41 @@ import matplotlib.image
 import numpy as np
 import time
 
+from src.ray import ray
 from src.vec3 import vec3
 from src.util import write_color
 
-IMAGE_WIDTH = 256
-IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 400
+VIEWPORT_HEIGHT = 2
+ASPECT_RATIO = [16., 9.]
 CONST_BLUE = 0.25
 MAX_COLOR = 255
 
+def ray_color(r):
+    t = 0.5 * (r.direction.normalized().y() + 1.0)
+    return vec3(data = (1., 1., 1.)) * (1. - t) + vec3(data = (0.5, 0.7, 1.0)) * t
+
 if __name__ == '__main__':
-    out_shape = (IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+    aspect_ratio = ASPECT_RATIO[0] / ASPECT_RATIO[1]
+    # Image
+    image_height = int(IMAGE_WIDTH / aspect_ratio)
+    out_shape = (image_height, IMAGE_WIDTH, 3)
     data = np.zeros(shape=out_shape)
 
-    for y in range(IMAGE_HEIGHT):
+    # Camera
+    viewport_width = aspect_ratio * VIEWPORT_HEIGHT
+    focal_length = 1
+    origin = vec3(data = (0., 0., 0.))
+    horizontal = vec3(data = (viewport_width, 0., 0.))
+    vertical = vec3(data = (0., VIEWPORT_HEIGHT, 0.))
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(data = (0, 0, focal_length))
+
+    for y in range(image_height):
         for x in range(IMAGE_WIDTH):
-            write_color(data, y, x, vec3(data = ((IMAGE_HEIGHT - y) / IMAGE_HEIGHT, x / IMAGE_WIDTH, CONST_BLUE)))
+            u = x / IMAGE_WIDTH
+            v = (image_height - y) / image_height
+            r = ray(origin, lower_left_corner + horizontal * u + vertical * v - origin)
+            color = ray_color(r)
+            write_color(data, y, x, color)
 
     matplotlib.image.imsave('out.png', data)
---
+++ src/ray.py
@@ -0,0 +1,9 @@
+from src.vec3 import vec3
+
+class ray:
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
+
+    def at(self, t):
+        return self.origin + t * self.direction
